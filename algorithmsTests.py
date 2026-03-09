e13cd2f3263aa79de7baf59b2e55021eb8b4e286
CosmicWatermeln <165712076+CosmicWatermeln@users.noreply.github.com>
2026-03-08 19:11:16 -0600

    Begin on algorithmTests.py

--- algorithmsTests.py
+++ algorithmsTests.py
@@ -6,18 +6,27 @@ Primary author: Christian Miller
 """
 
 import algorithms
+from algorithms import AlgorithmReport
 
-def testDriver():
+def testDriver(n : int) -> list[AlgorithmReport]:
     """
-    driver program that calls your functions from Part 2 and the generator from Part 1. Then runs all four algorithms on each permutation, and records:
+    driver program that calls your functions from Part 2 and the generator from Part 1. 
+    Then runs all four algorithms on each permutation, and records:
 
-    Algorithm name
-    The unsorted array used
-    Number of comparisons
+    Returns:
+        Algorithm name
+
+        The unsorted array used
+
+        Number of comparisons
     
     """
-    #TODO UPDATE COMMENT
+    #TODO UPDATE COMMENT (I can do this, don't do it for me)
     #TODO IMPLEMENT
+    algorithms.generateArrayOfAllPossiblePermutations(n) #is there a better name for this?
+
+
+    return None
     pass
 
 def experimentalRuns():
@@ -25,13 +34,20 @@ def experimentalRuns():
     Run your system for n = 4, 6, 8 and record for each algorithm:
 
     Best 10 cases (fewest comparisons) and the input arrays that produced them
+
     Worst 10 cases (most comparisons) and the input arrays that produced them
+        
     Average comparisons across all permutations
 
     """
-    #TODO UPDATE COMMENT
+    #TODO UPDATE COMMENT (I can do this, don't do it for me)
     #TODO IMPLEMENT
+
+    testDriver(4)
+    testDriver(6)
+    testDriver(8)
+
     pass
 
 if __name__=="__main__":
-    pass
+    experimentalRuns()
