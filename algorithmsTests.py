51861dd3b8c9a099971e614563725796b681152e
CosmicWatermeln <165712076+CosmicWatermeln@users.noreply.github.com>
2026-03-08 19:00:17 -0600

    Create algorithmsTests.py

---
+++ algorithmsTests.py
@@ -0,0 +1,37 @@
+"""
+Ethan J. Hadley, Chris Miller, Christopher Reynolds, and Theodore Tran
+CS 2430 Team 6
+Programming Project 2: Algorithm Performance_PLO-CS-3 Spring 2026
+Primary author: Christian Miller
+"""
+
+import algorithms
+
+def testDriver():
+    """
+    driver program that calls your functions from Part 2 and the generator from Part 1. Then runs all four algorithms on each permutation, and records:
+
+    Algorithm name
+    The unsorted array used
+    Number of comparisons
+    
+    """
+    #TODO UPDATE COMMENT
+    #TODO IMPLEMENT
+    pass
+
+def experimentalRuns():
+    """
+    Run your system for n = 4, 6, 8 and record for each algorithm:
+
+    Best 10 cases (fewest comparisons) and the input arrays that produced them
+    Worst 10 cases (most comparisons) and the input arrays that produced them
+    Average comparisons across all permutations
+
+    """
+    #TODO UPDATE COMMENT
+    #TODO IMPLEMENT
+    pass
+
+if __name__=="__main__":
+    pass
